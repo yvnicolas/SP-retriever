7621d2d205cf65ee6966cb95ce6ddd0cf40c3eef
Yves Nicolas <yves.nicolas@dynamease.com>
2014-01-09 12:02:00 +0100

    First level link finding ok

--- src/main/java/com/dynamease/serviceproviders/CpainsConnectionRetriever.java
+++ src/main/java/com/dynamease/serviceproviders/CpainsConnectionRetriever.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
  * 
  */
 public class CpainsConnectionRetriever implements SPConnectionRetriever {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(CpainsConnectionRetriever.class);
 
     private final String DIRURL = "http://copainsdavant.linternaute.com/glossary/users/";
@@ -107,21 +107,167 @@ public class CpainsConnectionRetriever implements SPConnectionRetriever {
     @Override
     public List<SpInfoPerson> getPersonInfo(Person person) throws SpInfoRetrievingException {
 
-       
         String url = DIRURL + person.getLastName().toLowerCase().substring(0, 1);
         try {
             Document doc = Jsoup.connect(url).get();
+
+            // Get the main directory list page for the first letter of the last name
             Element lettre2 = doc.getElementsByClass("listelement").get(0);
             Elements lettre = lettre2.getElementsByTag("li");
-            System.out.println(String.format("%s entrees trouvees pour lettre %s", lettre.size(), person.getLastName().toLowerCase().substring(0, 1)));
-            logger.debug(String.format("%s entrees trouvees pour lettre %s", lettre.size(), person.getLastName().toLowerCase().substring(0, 1)));
+            logger.debug(String.format("%s entrees trouvees pour lettre %s", lettre.size(), person.getLastName()
+                    .toLowerCase().substring(0, 1)));
+
+            // Find the proper link to follow
+            String sublink = findLetterSublink(lettre, person, 0, lettre.size());
+            logger.debug(String.format("Corresponding link : %s", sublink));
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-       
 
         return null;
     }
 
+    /**
+     * On the letter page of the copains d'avant directory, there is an ordered list of links which
+     * html code example is
+     * 
+     * <li>
+     * <a href="/glossary/users/n-19"> <strong>NEMER Philippe</strong> à <strong>NERON
+     * Damien</strong> </a></li>
+     * 
+     * 
+     * This private method finds the right link to use for the @param Person in the liste of several
+     * <li>entries similar to above example,returns the link, in the above example would be
+     * "/glossary/users/n-19"
+     * 
+     * Used indexes as argument as first tries with only lists resulted in following exception:
+     * java.lang.ClassCastException: java.util.ArrayList$SubList cannot be cast to
+     * org.jsoup.select.Elements
+     * 
+     * @param liste
+     * @param person
+     * @return
+     */
+    private String findLetterSublink(Elements liste, Person person, int start, int end) {
+
+        logger.debug(String.format("appel findLetterSubLink. Start = %s, End = %s", start, end));
+        int size = end - start;
+        // if liste is empty : we are out of bound, name is not present
+        if (size == 0) {
+            return null;
+        }
+
+        // if liste has only one element, we check the name is inside, if not, no match
+        if (size == 1) {
+            Element e = liste.get(start);
+            if (comparePerson(person, e) == 0)
+                return liste.get(start).getElementsByTag("a").get(0).attr("href");
+            else
+                return null;
+        }
+
+        // liste has at least 2 elements.
+        // Pick the middle element
+        int middleIndex = start + (int) Math.floor(size / 2);
+        Element middle = liste.get(middleIndex);
+
+        // Compare it to lastName
+        int compare = comparePerson(person, middle);
+
+        // lucky we are : it is the right one
+        if (compare == 0) {
+            return liste.get(middleIndex).getElementsByTag("a").get(0).attr("href");
+        }
+
+        // Choose the right part of the list on which to recursively apply the algorithm depending
+        // on the comparison
+        if (compare < 0) {
+            return (findLetterSublink(liste, person, start, middleIndex));
+        } else {
+            return (findLetterSublink(liste, person, middleIndex + 1, end));
+        }
+    }
+
+    /**
+     * element looks like this <li><a href="/glossary/users/n-19"> <strong>NEMER Philippe</strong> à
+     * <strong>NERON Damien</strong> </a></li>
+     * 
+     * returns 0 it the person fits inside the range, -1 if it is before, 0, if it is after
+     * 
+     * @param person
+     * @param element
+     * @return
+     */
+    private int comparePerson(Person person, Element element) {
+
+        Elements names = element.getElementsByTag("strong");
+        String first = names.get(0).text();
+
+        int compareToFirst = comparePerson(person, first);
+        if (compareToFirst == 0) {
+            // person equals the first of the 2 names, there is a match
+            return 0;
+        } else if (compareToFirst == -1) {
+            // before the first one means before the intervall
+            return -1;
+        }
+
+        // We are strictly after the first one, we compare to the last
+        String last = names.get(1).text();
+        int compareToLast = comparePerson(person, last);
+        if (compareToLast == -1) {
+            // After the first and before the last : there is a match
+            return 0;
+        }
+        if (compareToLast == 0) {
+            // Equals to last : there is a match
+            return 0;
+        }
+
+        // At this stage, the person is after the interval
+        return 1;
+    }
+
+    /**
+     * example of name would be NEMER Philippe returns 0 if the person matches the name, -1 if
+     * before, 1 if after
+     * 
+     * @param person
+     * @param name
+     * @return
+     */
+     private int comparePerson(Person person, String name) {
+         String last = extractLast(name);
+         String first = extractFirst(name);
+  
+        int compareToLast = person.getLastName().toLowerCase().compareTo(last.toLowerCase());
+        if (compareToLast < 0)
+            return -1;
+        if (compareToLast > 0)
+            return 1;
+
+        // At this stage, equality of last names, we need to compare to first name.
+        int compareToFirst = person.getFirstName().toLowerCase().compareTo(first.toLowerCase());
+        if (compareToFirst < 0)
+            return -1;
+        if (compareToFirst > 0)
+            return 1;
+
+        // reaching this point means equality.
+        return 0;
+    }
+
+      String extractFirst(String name) {
+         String last = extractLast(name);
+         String toReturn = name.replaceFirst(last+"\\s","");
+//         logger.debug(String.format("Extracting First name from %s : %s", name, toReturn));
+         return toReturn;
+     }
+     
+     String extractLast(String name) {
+         String toReturn = name.replaceFirst("\\s[A-Z][a-z].*", "");
+//         logger.debug(String.format("Extracting Last Name from %s : %s", name, toReturn));
+         return toReturn;
+     }
 }
--- src/test/java/com/dynamease/serviceproviders/CpainsConnectionRetrieverTest.java
+++ src/test/java/com/dynamease/serviceproviders/CpainsConnectionRetrieverTest.java
@@ -2,16 +2,59 @@ package com.dynamease.serviceproviders;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CpainsConnectionRetrieverTest {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(CpainsConnectionRetrieverTest.class);
     private static final CpainsConnectionRetriever underTest = new CpainsConnectionRetriever();
 
     @Test
     public void testGetPersonInfo() throws SpInfoRetrievingException {
-       Person p = new Person("Yves", "Nicolas");
-       assertNull(underTest.getPersonInfo(p));
+        Person p = new Person("Yves", "Nicolas");
+        assertNull(underTest.getPersonInfo(p));
+
+        // check somebody in the first chunk
+        p = new Person("François", "Nabur");
+        assertNull(underTest.getPersonInfo(p));
+
+        // check somebody before
+        p = new Person("Arthur", "N.");
+        assertNull(underTest.getPersonInfo(p));
+
+        // check the last one
+        p = new Person("Jerome", "Nuxa");
+        assertNull(underTest.getPersonInfo(p));
+
+        // check post last one
+        p = new Person("Jerome", "Nzz");
+        assertNull(underTest.getPersonInfo(p));
+
+        // check Equality to last
+        p = new Person("Dhakino", "Nzwange");
+        assertNull(underTest.getPersonInfo(p));
+
+        // check Equality to first
+        p = new Person("Audrey", "N.");
+        assertNull(underTest.getPersonInfo(p));
+
+        // cas des espaces dans les noms
+        p = new Person("Thierry", "Nait");
+        assertNull(underTest.getPersonInfo(p));
+
+    }
+
+    @Test
+    public void testExtractName() {
+        assertTrue(underTest.extractLast("NICOLAS Yves").equals("NICOLAS"));
+        assertTrue(underTest.extractFirst("NICOLAS Yves").equals("Yves"));
+        assertTrue(underTest.extractLast("NAIT SAID Hakim").equals("NAIT SAID"));
+        assertTrue(underTest.extractFirst("NAIT SAID Hakim").equals("Hakim"));
+        assertTrue(underTest.extractLast("NKELETELA BANZOUZI Ngounga ines").equals("NKELETELA BANZOUZI"));
+        assertTrue(underTest.extractFirst("NKELETELA BANZOUZI Ngounga ines").equals("Ngounga ines"));
     }
 
 }
